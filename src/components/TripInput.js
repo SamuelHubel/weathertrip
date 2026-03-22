13393e7c37b34ef3caa0e13fb6e95c7b64f92281
SamExclamationPoint <126426968+SamExclamationPoint@users.noreply.github.com>
2026-03-21 23:08:40 -0600

    shell tripInput and tripMap
    begin empty shell for map display and trip plotting

---
+++ src/components/TripInput.js
@@ -0,0 +1,17 @@
+// component to input trip details and add to list
+// need to add submit buttne
+import React, { useState } from 'react';
+
+function TripInput({ addTrip }) {
+  // trip info
+    const [tripName, setTripName] = useState('');
+    const [tripDate, setTripDate] = useState('');
+    const [tripStartLocation, setTripStartLocation] = useState('');
+    const [tripEndLocation, setTripEndLocation] = useState('');
+    // handle form submission
+
+
+}
+
+
+export default TripInput;  
