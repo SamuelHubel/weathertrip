5de3472da82b6caa273023bc2602b77e07186e90
SamExclamationPoint <126426968+SamExclamationPoint@users.noreply.github.com>
2026-03-23 15:13:56 -0600

    Display world map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import TripMap from './components/TripMap';
-import TripList from './components/TripList';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather Trip</h1>
       </header>
+      <main>
+        <TripMap />
+      </main>
     </div>
   );
 }
